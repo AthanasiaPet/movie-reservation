c25e478b473ba93dd7c3620e12e288f34364a335
AthanasiaPet <athanasia.petsa@yahoo.com>
2025-12-16 20:08:03 +0200

    feat: add Movie entity

---
+++ src/main/java/gr/aueb/cf/moviereservation/model/Movie.java
@@ -0,0 +1,45 @@
+package gr.aueb.cf.moviereservation.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uuid;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer duration;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @ColumnDefault("true")
+    private Boolean isActive;
+
+    @PrePersist
+    public void initializeUUID() {
+        if (uuid == null) uuid = UUID.randomUUID().toString();
+
+    }
+
+}
