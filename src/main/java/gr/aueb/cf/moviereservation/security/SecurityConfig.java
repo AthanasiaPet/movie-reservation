fec5859e4a8356a6cdbf8fec4641163e3759fcea
AthanasiaPet <athanasia.petsa@yahoo.com>
2025-12-22 01:31:40 +0200

    chore: add request matcher for endpoint access

--- src/main/java/gr/aueb/cf/moviereservation/security/SecurityConfig.java
+++ src/main/java/gr/aueb/cf/moviereservation/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                             .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
                             .requestMatchers("/api/auth/login").permitAll()
+                            .requestMatchers("/api/movies").permitAll()
                             .requestMatchers("/api/users/**").hasAnyRole("ADMIN", "USER")
                             .requestMatchers("/**").authenticated()
                         )
