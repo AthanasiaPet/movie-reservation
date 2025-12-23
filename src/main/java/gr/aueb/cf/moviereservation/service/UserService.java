98d55e314f304286122be5b31a6b1054644b793b
AthanasiaPet <athanasia.petsa@yahoo.com>
2025-12-23 19:51:56 +0200

    refactor: add helper methods to UserService

--- src/main/java/gr/aueb/cf/moviereservation/service/UserService.java
+++ src/main/java/gr/aueb/cf/moviereservation/service/UserService.java
@@ -1,12 +1,14 @@
 package gr.aueb.cf.moviereservation.service;
 
 import gr.aueb.cf.moviereservation.core.enums.Role;
-import gr.aueb.cf.moviereservation.dto.UserRegisterRequestDTO;
+import gr.aueb.cf.moviereservation.dto.UserRegisterDTO;
 import gr.aueb.cf.moviereservation.model.User;
 import gr.aueb.cf.moviereservation.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +17,12 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public User register(UserRegisterRequestDTO dto) {
+    public User register(UserRegisterDTO dto) {
 
         User user = User.builder()
-                .username(dto.getUsername())
-                .email(dto.getEmail())
-                .password(passwordEncoder.encode(dto.getPassword()))
+                .username(dto.username())
+                .email(dto.email())
+                .password(passwordEncoder.encode(dto.password()))
                 .role(Role.USER)
                 .isActive(true)
                 .build();
@@ -28,6 +30,13 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 
 
 }
